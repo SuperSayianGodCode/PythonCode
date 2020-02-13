7503baf52c665be4cdd9b69db4f05a62cd727456
SuperSayianGodCode <60323723+SuperSayianGodCode@users.noreply.github.com>
2020-02-13 22:25:17 +0530

    GCD Program for variable length input
    This a python implementation of how to calculate GCD of variable length input.

---
+++ varlengthgcd.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 19:49:44 2020
+
+@author: rahul
+"""
+
+def gcd(a,b):
+#    print("Inside gcd func:a=",a," b=",b)
+    if len(b)==1:
+#        print("inside if:",b)
+#        print("len(b):",len(b))
+        if(b[0]==0): 
+            return a 
+        else: 
+#            tmp=a%b[0]
+#            res = [int(x) for x in str(tmp)] 
+            return gcd(b[0],[a%b[0]])
+    elif len(b)==0:
+        return a
+    else:
+#        print("Inside else:a=",a," b=",b)
+#        print("len(b):",len(b))
+        return gcd(b[0],b[1:])
+        
+        
+print(gcd(3,[4,29,64,11,73]))
